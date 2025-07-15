babeb407d9316b396c9bf25568b86639cac2d907
shreyansh <shreyanshmarshal2@gmail.com>
2025-07-16 05:03:38 +0530

    restful api design

---
+++ rest api/rest.js
@@ -0,0 +1,7 @@
+// RESTful API
+//type of api which have some rules or some best practices
+
+
+//It works on server client archietecture -> this means that the server and client should be independent of each other so the server sends the data in raw form to the client
+
+//always respect all http methods - > so here if we are getting get method then we should retrieve data and return that if it is post then it should send data and if using patch then it should update the user. but think about post updateuser this will do the work but it is not the best practice
